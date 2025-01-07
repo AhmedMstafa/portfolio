386a453c87fd20d0b3d7a5c91403d264386e7db8
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-01-07 10:12:34 +0200

    Some enhancement

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-// const loading = document.querySelector('.loading');
-// const loadingTitle = loading.querySelector('.loading-title');
-// const loadingProgress = loading.querySelector('.loading-progress span');
-// const seconds = 3000;
+const loading = document.querySelector('.loading');
+const loadingTitle = loading.querySelector('.loading-title');
+const loadingProgress = loading.querySelector('.loading-progress span');
+const seconds = 3000;
 
-// console.log(loadingProgress);
+console.log(loadingProgress);
 
-// function loadingProgressHandler() {
-//   loading.classList.remove('d-none');
+function loadingProgressHandler() {
+  loading.classList.remove('d-none');
 
-//   let counter = 0;
-//   const interval = setInterval(() => {
-//     loadingTitle.innerText = `${counter}%`;
-//     loadingProgress.style.width = `${counter}%`;
-//     counter += 1;
-//   }, 30);
+  let counter = 0;
+  const interval = setInterval(() => {
+    loadingTitle.innerText = `${counter}%`;
+    loadingProgress.style.width = `${counter}%`;
+    counter += 1;
+  }, 30);
 
-//   setTimeout(() => {
-//     clearInterval(interval);
-//     loading.classList.add('d-none');
-//   }, seconds);
-// }
+  setTimeout(() => {
+    clearInterval(interval);
+    loading.classList.add('d-none');
+  }, seconds);
+}
 
-// loadingProgressHandler();
+loadingProgressHandler();
 
 // project list
 const worksBtns = document.querySelectorAll('.works .nav-btn');
