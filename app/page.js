f6dcb6f27c49fb7a90387974d036136f14d9a8dd
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-01-21 23:43:19 +0200

    Add main background

--- app/page.js
+++ app/page.js
@@ -2,5 +2,5 @@ import styles from './page.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 export default function Home() {
-  return <FontAwesomeIcon icon={faHouse} />;
+  return <FontAwesomeIcon className="d-inline" icon={faHouse} />;
 }
