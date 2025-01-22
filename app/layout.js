d36fb2acc5053a4ce8bddf2a5cde6f28e78170fd
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-01-22 04:21:18 +0200

    Add loading page

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,9 @@ import { Inter } from 'next/font/google';
 import 'bootstrap/dist/css/bootstrap.css';
 import './globals.css';
 import BootstrapClient from '@/lib/bootstrap-client';
-
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { Suspense } from 'react';
 config.autoAddCss = false;
 
 const inter = Inter({
@@ -33,8 +33,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable}`}>
-        {children}
         <BootstrapClient />
+        {children}
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
-import styles from './page.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import Loading from './loading/page';
+
 export default function Home() {
-  return <FontAwesomeIcon className="d-inline" icon={faHouse} />;
+  return <Loading />;
 }
