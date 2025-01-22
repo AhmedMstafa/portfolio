0fae145d5c0eb6154e31293b100ab228fb7bb40a
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-01-22 07:45:38 +0200

    Edit fonts and colors

--- components/hero.js
+++ components/hero.js
@@ -4,10 +4,10 @@ export default function Hero() {
   return (
     <section className="hero vh-100">
       <div className="container ps-4 pe-4 h-100 d-flex flex-column justify-content-center ">
-        <div className="greet fs-5 fs-md-4">
+        <div className="greet fs-md-4">
           <span>HELLO THERE!</span>
         </div>
-        <div className="title pt-4 pb-4 ">
+        <div className="title lh-1 pt-2 pb-2 ">
           <span>
             I&apos;m Ahmed Mostafa,
             <br />a creative Frontend <br />
