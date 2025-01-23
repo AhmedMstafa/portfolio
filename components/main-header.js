388dbc415a3119d0a48e890cd9a4cca2b5d7f3dd
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-01-23 19:40:09 +0200

    Edit main-header

--- components/main-header.js
+++ components/main-header.js
@@ -1,28 +1,33 @@
 'use client';
-import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function MainHeader({ title }) {
-  useEffect(() => {
-    gsap.fromTo(
-      '.about-text',
-      { x: -100 },
-      {
-        x: 0,
-        duration: 1,
-        scrollTrigger: {
-          trigger: '.about-text',
-          start: 'top 90%',
-          end: 'top 0%',
-        },
-      }
-    );
-  }, []);
+  const container = useRef();
+  useGSAP(
+    () => {
+      gsap.fromTo(
+        '.about-text',
+        { x: -100 },
+        {
+          x: 0,
+          duration: 1,
+          scrollTrigger: {
+            trigger: '.about-text',
+            start: 'top 90%',
+            end: 'top 0%',
+          },
+        }
+      );
+    },
+    { scope: container }
+  );
 
   return (
-    <div className="section-header p-0">
+    <div ref={container} className="section-header p-0">
       <div className="text fw-semibold d-block mb-3 p-0">
         <span className="about-text fs-3">{title}</span>
       </div>
